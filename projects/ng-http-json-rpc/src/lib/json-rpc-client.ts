90bec45dd3ae219d5f5815fad1e3b13bd2c378a3
Shawn Shaddock <shawnshaddock@gmail.com>
2018-10-14 21:38:24 -0400

    Added extensionData.

--- projects/ng-http-json-rpc/src/lib/json-rpc-client.ts
+++ projects/ng-http-json-rpc/src/lib/json-rpc-client.ts
@@ -10,12 +10,12 @@ import { JsonRpcResponse } from './json-rpc-response';
 export class JsonRpcClient {
     constructor(private http: HttpClient) { }
 
-    async sendRequest<T>(url: string, method: string, params: any = null, id: any = null) {
+    async send<T>(url: string, method: string, params: any = null, id: any = null, extensionData: any = null) {
         let request = new JsonRpcRequest();
-
         request.method = method;
         request.params = params;
         request.id = id;
+        request = {...request, ...extensionData};
 
         let item = await this.http.post<JsonRpcResponse<T>>(url, request).toPromise();
 
